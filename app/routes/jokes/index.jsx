0f7241179255020498c215bd68b8845ea07e4bee
Mark Huggins <mhuggins@glgroup.com>
2022-02-08 21:42:14 -0500

    trying use fetcher

--- app/routes/jokes/index.jsx
+++ app/routes/jokes/index.jsx
@@ -1,46 +1,44 @@
-import { useLoaderData, Form, useSubmit, redirect } from "remix";
-export let loader = async ({request}) => {
-  const url = new URL(request.url);
-  console.log(url)
-  const category = url.searchParams.get("category") || "dev";
-  console.log(category)
-  const [jokesRes, categoriesRes] = await Promise.all([
-    fetch(`https://api.chucknorris.io/jokes/random?category=${category}`),
-    fetch(`https://api.chucknorris.io/jokes/categories`),
-  ]);
+import { useLoaderData, Form, redirect useFetcher} from 'remix'
+export async function loader({ request }) {
+    const url = new URL(request.url)
+    console.log(url)
+    const category = url.searchParams.get('category') || 'dev'
+    console.log(category)
+    const [jokesRes, categoriesRes] = await Promise.all([
+        fetch(`https://api.chucknorris.io/jokes/random?category=${category}`),
+        fetch(`https://api.chucknorris.io/jokes/categories`),
+    ])
 
-  const joke = await jokesRes.json();
-  const categories = await categoriesRes.json();
-  const filteredCategories = categories.filter((c) => c !== "explicit");
-  return { joke, filteredCategories };
-};
+    const joke = await jokesRes.json()
+    const categories = await categoriesRes.json()
+    const filteredCategories = categories.filter((c) => c !== 'explicit')
+    return { joke, filteredCategories }
+}
 
-export let action = async ({ request }) => {
-  const formData = await request.formData();
-  const category = formData['_fields']['category'][0]
-  return redirect(`/jokes?category=${category}`);
+export async function action({ request }) {
+    const formData = await request.formData()
+    const category = formData._fields.category[0]
+    return 
 }
 export default function JokesIndexRoute() {
-  const submit = useSubmit();
-  function handleChange(event) {
-    console.log(event);
-    submit(event.currentTarget, { replace: true });
-  }
-  const { joke, filteredCategories } = useLoaderData();
-  return (
-    <div>
-      <Form method="post" onChange={handleChange}>
-        <label for="category">Choose a category:</label>
-        <select name="category" id="category">
-          <option disabled selected>Pick a Category</option>
-          {filteredCategories.map((c) => {
-            return <option value={c}>{c}</option>;
-          })}
-        </select>
-        <button type="submit">Submit</button>
-      </Form>
-      <p>Here's a random joke:</p>
-      <p>{joke.value}</p>
-    </div>
-  );
+  const fetcher = useFetcher()
+    const { joke, filteredCategories } = useLoaderData()
+    return (
+        <div>
+            <fetcher.Form method="post">
+                <label htmlFor="category">Choose a category:</label>
+                <select name="category" id="category">
+                    <option disabled selected>
+                        Pick a Category
+                    </option>
+                    {filteredCategories.map((c) => {
+                        return <option value={c}>{c}</option>
+                    })}
+                </select>
+                <button type="submit">Submit</button>
+      </fetcher.Form>
+            <p>Here's a random joke:</p>
+            <p>{joke.value}</p>
+        </div>
+    )
 }
